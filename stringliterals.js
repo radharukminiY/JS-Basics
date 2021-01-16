7b8b6b1c60cc7d16ccc54e556d2e547cc82fd30a
radharukminiY <rukku558@gmail.com>
2021-01-16 23:04:55 +0530

    commit 4

---
+++ stringliterals.js
@@ -0,0 +1,11 @@
+var name="bahubali"
+var rating =4.9
+var type= "History"
+var industry="tollywood(indian movie)"
+
+var out="The "+name+" is an "+type+" movies, with rating of "+rating+" and from industry "+industry+" ."
+
+
+var out = `The ${name} is an ${type} movie with rating of ${rating} and from industry ${industry}.`
+
+//The bahubali is an History movies, with rating of 4.9 and from industry tollywood(indian movie)."

9be01e45e919dd68c89a29750d9cfbee9e7fff87
radharukminiY <rukku558@gmail.com>
2021-01-16 22:57:05 +0530

    commit 3

---
+++ conditions.js
@@ -0,0 +1,68 @@
+var a=5
+var b="5"
+var c=5
+
+a==b
+true
+a===b
+false  //if == is false === will be false
+
+////
+var x="Bonjour"
+var y="bonjour"
+undefined
+a==b (bcz of case sensitive)
+false
+a===b
+false
+
+//Negation
+ var a=true
+ undefined
+ !a
+ false
+var b= !a
+undefined
+b
+false
+!b
+true
+
+var x=1
+!x
+false
+var y=0
+!y
+true
+var c= 4
+!c
+false
+ var u=-10
+ !u
+ false
+
+//truthy = any number expect 0 weather +ve or -ve  is true, true, any string
+//falsy = 0, null, false, undefined
+>
+<
+>=
+<=
+!=
+!==
+
+
+ //comparison operator always return boolean value
+var x>5
+false
+a<3
+false
+b==4
+true
+c!=3
+false
+c!==5
+false
+c>=9
+true
+c<=5
+true
